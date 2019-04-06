ed48e1c59a4d32f3306548bee72476f674a65870
hazem osama ibrahim <hazem3500@gmail.com>
2019-04-06 08:38:49 +0200

    added linked allocation implementation

--- helpers/allocationMethods.mjs
+++ helpers/allocationMethods.mjs
@@ -3,7 +3,7 @@ const contiguous = {
         const largestSpace = getLargestEmptySpace(blocks);
         if (largestSpace.size < file.size) throw Error('Out of memory!');
         file.blocks.push({ start: largestSpace.start, size: file.size });
-        this.setSpaceValue(
+        setSpaceValue(
             blocks,
             { start: largestSpace.start, size: file.size },
             true
@@ -11,20 +11,83 @@ const contiguous = {
     },
     deallocate(blocks, file) {
         if (!file.blocks) throw Error('File not allocated!');
-        this.setSpaceValue(blocks, file.blocks[0], false);
-    },
-
-    setSpaceValue(blocks, { start, size }, value) {
-        for (let i = start; i < start + size; i++) {
-            blocks[i] = value;
-        }
+        setSpaceValue(blocks, file.blocks[0], false);
     }
 };
 
 const linked = {
+    allocate(blocks, file) {
+        const freeSpace = getFreeSpace(blocks);
+        const freeSpaceLeft = freeSpace.reduce(
+            (size, space) => size + space.size,
+            0
+        );
+        if (freeSpaceLeft < file.size) throw Error('Out of memory!');
+
+        let firstAllocatedSpace;
+        let remainingFileSize = file.size;
+
+        for (let i = 0; i < freeSpace.length; i++) {
+            if (freeSpace[i].size < remainingFileSize) {
+                addAllocatedSpace({
+                    start: freeSpace[i].start,
+                    size: freeSpace[i].size,
+                    nextSpace: null
+                });
+                remainingFileSize -= freeSpace[i].size;
+            } else {
+                addAllocatedSpace({
+                    start: freeSpace[i].start,
+                    size: remainingFileSize,
+                    nextSpace: null
+                });
+                break;
+            }
+        }
+
+        file.blocks.push(firstAllocatedSpace);
+
+        setAllocatedSpaceValues();
+
+        function setAllocatedSpaceValues() {
+            let currAllocatedSpace = firstAllocatedSpace;
+            do {
+                setSpaceValue(blocks, currAllocatedSpace, true);
+                if (currAllocatedSpace.nextSpace) {
+                    currAllocatedSpace = currAllocatedSpace.nextSpace;
+                }
+            } while (currAllocatedSpace.nextSpace);
+        }
+
+        function addAllocatedSpace(space) {
+            if (!firstAllocatedSpace) {
+                firstAllocatedSpace = space;
+                return;
+            }
+            let currAllocatedSpace = firstAllocatedSpace;
+            while (currAllocatedSpace.nextSpace) {
+                currAllocatedSpace = currAllocatedSpace.nextSpace;
+            }
+            currAllocatedSpace.nextSpace = space;
+        }
+    },
+
+    deallocate(blocks, file) {
+        if (!file.blocks) throw Error('File not allocated!');
+
+        let currAllocatedSpace = file.blocks[0];
+        do {
+            setSpaceValue(blocks, currAllocatedSpace, false);
+            if (currAllocatedSpace.nextSpace) {
+                currAllocatedSpace = currAllocatedSpace.nextSpace;
+            }
+        } while (currAllocatedSpace.nextSpace);
+    }
+};
+
+const indexed = {
     allocate(blocks, file) {},
-    deallocate(blocks, file) {},
-    setSpaceValue(blocks, { start, size }, value) {}
+    deallocate(blocks, file) {}
 };
 
 export default function allocationStrategy(allocationType) {
@@ -35,11 +98,7 @@ export default function allocationStrategy(allocationType) {
         return linked;
     }
     if (allocationType === 'indexed') {
-        return {
-            allocate: null,
-            deallocate: null,
-            setSpaceValue: null
-        };
+        return indexed;
     }
 }
 export function getFreeSpace(blocks) {
@@ -83,4 +142,10 @@ export function getLargestEmptySpace(blocks) {
         },
         { size: 0 }
     );
+}
+
+function setSpaceValue(blocks, { start, size }, value) {
+    for (let i = start; i < start + size; i++) {
+        blocks[i] = value;
+    }
 }
