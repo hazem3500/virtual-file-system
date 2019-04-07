ba53f23d3ca00e105984c4767573711955394fbf
hazem osama ibrahim <hazem3500@gmail.com>
2019-04-07 21:22:06 +0200

    Added error tolerence to the code base

--- script.mjs
+++ script.mjs
@@ -26,46 +26,50 @@ async function run() {
         () => !exitApplication,
         () =>
             queryCommands().then(async (answers) => {
-                switch (answers.command) {
-                case 'createFile':
-                    {
-                        const {
-                            directory,
-                            size
-                        } = await queryDirectoryAndSize();
-                        fileSystem.createFile(directory, size);
+                try {
+                    switch (answers.command) {
+                    case 'createFile':
+                        {
+                            const {
+                                directory,
+                                size
+                            } = await queryDirectoryAndSize();
+                            fileSystem.createFile(directory, size);
+                        }
+                        break;
+                    case 'createFolder':
+                        {
+                            const { directory } = await queryDirectory();
+                            fileSystem.createFolder(directory);
+                        }
+                        break;
+                    case 'deleteFile':
+                        {
+                            const { directory } = await queryDirectory();
+                            fileSystem.deleteFile(directory);
+                        }
+                        break;
+                    case 'deleteFolder':
+                        {
+                            const { directory } = await queryDirectory();
+                            fileSystem.deleteFolder(directory);
+                        }
+                        break;
+                    case 'displayDiskStatus':
+                        fileSystem.displayDiskStatus();
+                        break;
+                    case 'displayDiskStructure':
+                        fileSystem.displayDiskStructure();
+                        break;
+                    case 'exit':
+                        exitApplication = true;
+                        kfs.fileSystem = JSON.stringify(fileSystem);
+                        process.exit(0);
+                        break;
+                    default:
                     }
-                    break;
-                case 'createFolder':
-                    {
-                        const { directory } = await queryDirectory();
-                        fileSystem.createFolder(directory);
-                    }
-                    break;
-                case 'deleteFile':
-                    {
-                        const { directory } = await queryDirectory();
-                        fileSystem.deleteFile(directory);
-                    }
-                    break;
-                case 'deleteFolder':
-                    {
-                        const { directory } = await queryDirectory();
-                        fileSystem.deleteFolder(directory);
-                    }
-                    break;
-                case 'displayDiskStatus':
-                    fileSystem.displayDiskStatus();
-                    break;
-                case 'displayDiskStructure':
-                    fileSystem.displayDiskStructure();
-                    break;
-                case 'exit':
-                    exitApplication = true;
-                    kfs.fileSystem = JSON.stringify(fileSystem);
-                    process.exit(0);
-                    break;
-                default:
+                } catch (err) {
+                    console.warn(err);
                 }
             })
     );
